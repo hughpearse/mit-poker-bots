5967bde60c9c824ff7f2120f3a7edaf2be59f5fa
HUGH Pearse <hughpear@ie.ibm.com>
2017-08-18 16:17:24 +0100

    added plaintext hand explanation at showdown in manual bot

--- client/manual-interface/Player.py
+++ client/manual-interface/Player.py
@@ -3,7 +3,7 @@ import socket
 import sys
 import logging
 import os
-
+from lib import Card, Deck, Evaluator
 
 """
 Simple example pokerbot, written in python.
@@ -212,11 +212,20 @@ class Player:
         
         print "Match finished: "
         action_command_list = []
+        evaluator = Evaluator()
+        matchSummary = ""
         for action in self.lastActions:
             actionArray = action.split(':')
             action_command_list.append(actionArray[0])
             if len(actionArray[0]) > 0:
                 print " " + str(actionArray)
+            if actionArray[0] == "SHOW" and self.numBoardCards == 5:
+                board = [Card.new(self.boardCards[0]), Card.new(self.boardCards[1]), Card.new(self.boardCards[2]), Card.new(self.boardCards[3]), Card.new(self.boardCards[4])]
+                hand = [Card.new(self.holeCards[0]), Card.new(self.holeCards[1])]
+                score = evaluator.evaluate(board, hand)
+                matchSummary += "Player: " + actionArray[3] + " had a " + evaluator.class_to_string(evaluator.get_rank_class(score)) + ". "
+        if len(matchSummary) > 0:
+            print matchSummary
         print "Cumulative change in bankroll (P/L): " + str(self.myBank)
 
     '''
